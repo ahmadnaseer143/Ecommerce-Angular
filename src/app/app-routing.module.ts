941f5ef847312e27eb94661c7f5c1fceb1be2402
Ahmad <Naseer.Ahmad2@corp.ibexglobal.com>
2023-06-13 00:05:29 -0700

    bugs fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
 import { OrderComponent } from './order/order.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'product-details', component: ProductComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'orders', component: OrderComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
