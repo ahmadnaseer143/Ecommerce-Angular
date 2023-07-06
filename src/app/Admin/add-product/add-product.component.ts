c48164f85873ad3933cd31d22d5116edc038d05c
Ahmad <Naseer.Ahmad2@corp.ibexglobal.com>
2023-07-06 02:21:18 -0700

    admin add product frontend

--- src/app/Admin/add-product/add-product.component.ts
+++ src/app/Admin/add-product/add-product.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/models';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+
+  productForm !: FormGroup;
+  constructor(private router: Router, private navigationService: NavigationService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.productForm = this.formBuilder.group({
+      id: [1, Validators.required],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [0, Validators.required],
+      productCategory: this.formBuilder.group({
+        id: [1, Validators.required],
+        category: ['', Validators.required],
+        subCategory: ['', Validators.required]
+      }),
+      offer: this.formBuilder.group({
+        id: [1, Validators.required],
+        title: ['', Validators.required],
+        discount: [0, Validators.required]
+      }),
+      quantity: [0, Validators.required],
+      imageName: ['', Validators.required]
+    })
+  }
+
+  addProduct() {
+    if (this.productForm.invalid) {
+      alert("Validation Error");
+      return;
+    }
+
+    const product = { ...this.productForm.value } as Product;
+
+    console.log(product);
+
+    this.router.navigate(['admin'])
+  }
 
 }
