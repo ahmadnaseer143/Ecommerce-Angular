4bd835286cff8441a12b23f246d4987d738496ca
ahmadnaseer143 <anaseer942@gmail.com>
2023-07-10 06:56:17 -0700

    admin editcategory frontend

--- src/app/Admin/categories/categories.component.ts
+++ src/app/Admin/categories/categories.component.ts
@@ -13,6 +13,7 @@ export class CategoriesComponent {
   categories: Category[] = [];
   categoryForm !: FormGroup;
   showFormValue: boolean = false;
+  edit: boolean = false;
 
   constructor(private navigationService: NavigationService, private formBuilder: FormBuilder) { }
 
@@ -36,7 +37,30 @@ export class CategoriesComponent {
   addCategory() {
     this.categoryForm.markAllAsTouched();
     if (this.categoryForm.invalid) return;
-    console.log(this.categoryForm.value);
+
+    if (this.edit) {
+      console.log("Edit Form");
+      console.log(this.categoryForm.value);
+    } else {
+      console.log("Add Form");
+      console.log(this.categoryForm.value);
+    }
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.edit = false;
+    this.categoryForm.reset();
+  }
+
+  editCategory(category: Category) {
+    this.edit = true;
+    this.showFormValue = true;
+    this.categoryForm.patchValue({
+      id: category.id,
+      category: category.category,
+      subCategory: category.subCategory,
+    })
   }
 
   showForm() {
