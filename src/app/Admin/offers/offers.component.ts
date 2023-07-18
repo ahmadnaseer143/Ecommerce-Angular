083a13e7cc45aaa11641c0137295cd346d8a85f0
ahmadnaseer143 <anaseer942@gmail.com>
2023-07-17 22:58:26 -0700

    offers frontend

--- src/app/Admin/offers/offers.component.ts
+++ src/app/Admin/offers/offers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-offers',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./offers.component.css']
 })
 export class OffersComponent {
+  offerForm !: FormGroup;
+  showFormValue: Boolean = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.offerForm = this.formBuilder.group({
+      id: [1, Validators.required],
+      title: ['', Validators.required],
+      discount: [0, Validators.required],
+    })
+
+    this.loadOffers();
+  }
+
+  loadOffers() {
+    // get all the offers
+  }
+
+  addOffer() {
+    // add offer
+  }
+
+  showForm() {
+    this.showFormValue = !this.showFormValue;
+  }
 
 }
